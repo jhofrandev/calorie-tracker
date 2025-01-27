959b6d4f760e19324088baece02c41052e17a8ee
Jhofran Briceño <jhofran.dev@outlook.com>
2025-01-27 10:39:27 -0500

    Trabajando con el state del formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,27 @@
+import { useState, ChangeEvent } from "react";
+
+import { Activity } from "../types";
+
 import { categories } from "../data/categories";
 
 export default function Form() {
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumber = ["category", "calories"].includes(e.target.id);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumber ? +e.target.value : e.target.value,
+    });
+  };
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -10,6 +31,8 @@ export default function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -20,14 +43,16 @@ export default function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de  Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -40,13 +65,15 @@ export default function Form() {
           type="number"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. 100"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
       <input
         type="submit"
         className="bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-        value="Guardar comida o Guardar Ejercicio"
+        value={"Agregar"}
       />
     </form>
   );
