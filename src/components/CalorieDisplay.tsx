1672f83dd93dd1f56e22313b4dda1552d6c2f8de
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-08 17:18:26 -0500

    add changes

--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -3,12 +3,13 @@ type CalorieDisplayProps = {
   text: string;
 };
 
-export default function CalorieDisplay({caloriasm text} : CalorieDisplayProps) {
+export default function CalorieDisplay({
+  calories,
+  text,
+}: CalorieDisplayProps) {
   return (
     <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-      <span className="font-black text-6xl text-orange-400">
-        {caloriasm}
-      </span>
+      <span className="font-black text-6xl text-orange-400">{calories}</span>
       {text}
     </p>
   );
