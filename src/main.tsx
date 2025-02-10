e28a254a9a4b0e3fd2734f535cd2d0093113f137
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-09 21:36:53 -0500

    add changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// Add
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
